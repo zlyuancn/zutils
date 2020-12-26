1ea094f24e4311103cf992b53d2f31b7f183ce51
zlyuancn <zlyuancn@163.com>
2020-12-26 17:03:43 +0800

    修复Reflect.IsZero多级struct对于Ptr的检测失误

--- reflect.go
+++ reflect.go
@@ -136,7 +136,7 @@ func (u *reflectUtil) structIsZero(r_v reflect.Value) bool {
 		if field.CanInterface() {
 			switch field.Kind() {
 			case reflect.Ptr, reflect.Interface:
-				if field.Interface() != nil {
+				if !field.IsNil() {
 					return false
 				}
 			default:
@@ -160,7 +160,7 @@ func (u *reflectUtil) structIsZero(r_v reflect.Value) bool {
 
 		switch field.Kind() {
 		case reflect.Ptr, reflect.Interface:
-			if temp.Elem().Interface() != nil {
+			if !temp.IsNil() {
 				return false
 			}
 		default:
