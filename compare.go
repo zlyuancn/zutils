7a48b6077f4c9fce41308ac41b85e26a3215b2d9
zlyuancn <zlyuancn@163.com>
2021-06-10 11:12:40 +0800

    (*compareUtil)

--- compare.go
+++ compare.go
@@ -5,7 +5,7 @@ var Compare = new(compareUtil)
 type compareUtil struct{}
 
 // 检查 a 是否在 values 中
-func CheckIn(a interface{}, values ...interface{}) bool {
+func (*compareUtil) CheckIn(a interface{}, values ...interface{}) bool {
 	for _, v := range values {
 		if v == a {
 			return true
@@ -15,7 +15,7 @@ func CheckIn(a interface{}, values ...interface{}) bool {
 }
 
 // 检查 a 是否在 values 中
-func CheckInInt(a int, values ...int) bool {
+func (*compareUtil) CheckInInt(a int, values ...int) bool {
 	for _, v := range values {
 		if v == a {
 			return true
@@ -25,7 +25,7 @@ func CheckInInt(a int, values ...int) bool {
 }
 
 // 检查 a 是否在 values 中
-func CheckInString(a string, values ...string) bool {
+func (*compareUtil) CheckInString(a string, values ...string) bool {
 	for _, v := range values {
 		if v == a {
 			return true
@@ -35,7 +35,7 @@ func CheckInString(a string, values ...string) bool {
 }
 
 // 比较两个字符串数组切片是否相等, 注意: 不会对nil值做比较, 也就是说 []string{} == nil
-func CompareStringSlice(a, b []string) bool {
+func (*compareUtil) CompareStringSlice(a, b []string) bool {
 	if len(a) != len(b) {
 		return false
 	}
