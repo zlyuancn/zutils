0b4cec26762f21cb11e7ee4b5d7f39d3da4ef940
zlyuancn <zlyuancn@163.com>
2021-09-24 16:51:36 +0800

    添加注释

--- redis.go
+++ redis.go
@@ -10,6 +10,7 @@ var Redis = new(redisUtil)
 
 type redisUtil struct{}
 
+// 比较和交换, 如果key的值等于v1, 则设为v2
 func (*redisUtil) Cas(ctx context.Context, client redis.UniversalClient, key, v1, v2 string) (bool, error) {
 	const script = `
   local v = redis.call("get", KEYS[1])
