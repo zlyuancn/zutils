f376aa4846f03db099bb736dbdbf6a36b3d54cc0
zlyuanzhang <zlyuanzhang@tencent.com>
2023-12-27 10:11:40 +0800

    atomic导出

--- atomic.go
+++ atomic.go
@@ -4,33 +4,33 @@ import (
 	"sync/atomic"
 )
 
-func NewAtomic[T any](init T) *atomicCli[T] {
-	a := &atomicCli[T]{
+func NewAtomic[T any](init T) *AtomicValue[T] {
+	a := &AtomicValue[T]{
 		value: atomic.Value{},
 	}
 	a.value.Store(init)
 	return a
 }
 
-type atomicCli[T any] struct {
+type AtomicValue[T any] struct {
 	value atomic.Value
 }
 
-func (a *atomicCli[T]) Set(t T) {
+func (a *AtomicValue[T]) Set(t T) {
 	a.value.Store(t)
 }
 
-func (a *atomicCli[T]) Get() T {
+func (a *AtomicValue[T]) Get() T {
 	v := a.value.Load()
 	ret, _ := v.(T)
 	return ret
 }
 
-func (a *atomicCli[T]) Swap(newV T) (oldV T) {
+func (a *AtomicValue[T]) Swap(newV T) (oldV T) {
 	v := a.value.Swap(newV)
 	ret, _ := v.(T)
 	return ret
 }
-func (a *atomicCli[T]) CAS(oldV T, newV T) (ok bool) {
+func (a *AtomicValue[T]) CAS(oldV T, newV T) (ok bool) {
 	return a.value.CompareAndSwap(oldV, newV)
 }
